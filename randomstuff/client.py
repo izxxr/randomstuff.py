359df123d4e7760b137756f9e989cb71d14ed4fd
NerdGuyAhmad <thisisizharahmad@gmail.com>
2021-05-27 19:19:10 +0500

    Add support for v4 beta

--- randomstuff/client.py
+++ randomstuff/client.py
@@ -45,7 +45,8 @@ class Client:
 		plan: str = None,
 		dev_name: str = 'PGamerX',
 		bot_name: str = 'RSA',
-		unique_id: str = None):
+		unique_id: str = ''
+		):
 		"""Gets AI response
 
 		Parameters:
@@ -70,11 +71,13 @@ class Client:
 			raise PlanError(F"Invalid Plan. Choose from {PLANS}")
 			return
 
-		if self.version == 'v2' and plan != None:
-			raise VersionError(f"v2 does not support {plan} plan.")
+		if self.version == 'v4':
+			raise VersionError("v4 is a beta version with entirely different endpoint and parameters. Please use `Client.get_ai_response_beta()` for v4 until beta is over.")
 			return
 
-
+		if self.version == 'v2' and plan != None:
+			raise VersionError(f"{self.version} does not support {plan} plan.")
+			return
 		
 		if self.version == 'v2':
 			response = self.session.get(f'{BASE_URL}/ai/response?message={message}&language={lang}&api_key={self.key}')
@@ -95,14 +98,72 @@ class Client:
 			return response.json()[0]['message']
 		
 		elif self.version == 'v3' and plan != None:
-			params = {'message': message, 'lang': lang, 'type': type}
 			response = self.session.get(f'{BASE_URL}/v3/{plan}/ai/response', params=params)
-			return response.text
 			if response.status_code == 401:
 				raise AuthError(response.text)
 				return
 			return response.json()[0]['message']
+
 		
+	def get_ai_response_beta(self,
+							message:str,
+							uid:str='',
+							language:str='en',
+							bot:str='RSA',
+							master:str='PGamerX',
+							server:str='primary'):
+		"""Gets AI response from v4 endpoint
+
+		[WARNING] v4 is a beta version with entirely different endpoint and parameters. Consider using v3 for
+		general use. Read the wiki for info about parameters of this function.
+
+		Parameters:
+			message (str): The message to which response is required
+			uid (str) (optional): This is used to save your identity in bot. Use a secure and combination of letters and numbers. Use `randomstuff.utils.generate_unique_id()` to generate one easily.
+			language (str) (optional): The language in which response is required. By default this is english.
+			bot (str) (optional): The bot's name. Used in responses.
+			master (str) (optional): The developer name. Used in responses.
+			server (str) (optional): The server from which the response should be obtained. Please see the table below
+		
+			|-----------|----------------------------------------------------------------|
+			|  Server   |                      Description                               |
+			|:---------:|:--------------------------------------------------------------:|
+			|  primary  |                     The main server.                           |
+			|  backup   | Since this is a beta, this is backup server if primary is down.|
+			|  unstable | The unstable server, Responses are very unstable in this one.  |
+			|-----------|----------------------------------------------------------------|
+
+		"""
+		if not self.version == 'v4':
+			raise VersionError("Only v4 supports this method.")
+			return
+
+		if not server in SERVERS:
+			raise ServerError("Invalid type of server provided. Only {SERVERS} are supported")
+			return
+
+		if server in ['backup', 'unstable'] and language != 'en':
+			raise ServerError(f"{server} does not support languages other then english.")
+			return
+
+		params = {'message': message,
+				'uid': uid,
+				'language': language,
+				'bot': bot,
+				'master': master,
+				'server': server
+			}
+		response = self.session.get(f'{BASE_URL}/beta/ai', params=params)
+
+		if response.status_code == 401:
+			raise AuthError(response.text)
+			return
+
+		if response.status_code == 403:
+			raise AuthError(response.text)
+			return
+
+		return response.json()[0]['message']
 		
 
 	def get_image(self, type: str = 'any'):
@@ -193,10 +254,10 @@ class AsyncClient:
 		message: str,
 		lang: str = 'en',
 		type: str = 'stable',
-		plan: str = None,
+		plan: str = '',
 		bot_name: str = 'RSA',
 		dev_name: str = 'PGamerX',
-		unique_id: str = None):
+		unique_id: str = ''):
 		"""
 		This function is a coroutine
 		----------------------------
@@ -254,6 +315,71 @@ class AsyncClient:
 				return
 
 			return (await response.json())[0]['message']
+
+	async def get_ai_response_beta(self,
+							message:str,
+							uid:str='',
+							language:str='en',
+							bot:str='RSA',
+							master:str='PGamerX',
+							server:str='primary'):
+		"""
+		This function is a coroutine
+		~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+		Gets AI response from v4 endpoint
+
+		[WARNING] v4 is a beta version with entirely different endpoint and parameters. Consider using v3 for
+		general use. Read the wiki for info about parameters of this function.
+
+		Parameters:
+			message (str): The message to which response is required
+			uid (str) (optional): This is used to save your identity in bot. Use a secure and combination of letters and numbers. Use `randomstuff.utils.generate_unique_id()` to generate one easily.
+			language (str) (optional): The language in which response is required. By default this is english.
+			bot (str) (optional): The bot's name. Used in responses.
+			master (str) (optional): The developer name. Used in responses.
+			server (str) (optional): The server from which the response should be obtained. Please see the table below
+		
+			|-----------|----------------------------------------------------------------|
+			|  Server   |                      Description                               |
+			|:---------:|:--------------------------------------------------------------:|
+			|  primary  |                     The main server.                           |
+			|  backup   | Since this is a beta, this is backup server if primary is down.|
+			|  unstable | The unstable server, Responses are very unstable in this one.  |
+			|-----------|----------------------------------------------------------------|
+
+		"""
+		if not self.version == 'v4':
+			raise VersionError("Only v4 supports this method.")
+			return
+
+		if not server in SERVERS:
+			raise ServerError("Invalid type of server provided. Only {SERVERS} are supported")
+			return
+
+		if server in ['backup', 'unstable'] and language != 'en':
+			raise ServerError(f"{server} does not support languages other then english.")
+			return
+
+
+		params = {'message': message,
+				'uid': uid,
+				'language': language,
+				'bot': bot,
+				'master': master,
+				'server': server
+			}
+		response = await self.session.get(f'{BASE_URL}/beta/ai', params=params)
+
+		if response.status == 401:
+			raise AuthError(response.text)
+			return
+
+		if response.status == 403:
+			raise AuthError(response.text)
+			return
+
+		return (await response.json())[0]['message']
 
 
 
