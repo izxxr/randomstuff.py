eb681a9abd2905ac25cc5b39753144e26632dcbb
NerdGuyAhmad <thisisizharahmad@gmail.com>
2021-05-27 19:27:48 +0500

    Added suppress_warnings info globally to _warn method

--- randomstuff/constants.py
+++ randomstuff/constants.py
@@ -3,6 +3,7 @@ def _warn(client, warning):
 		return
 	else:
 		print("[WARNING] "+"\n"+warning)
+		print("\n[INFO] You can disable warnings by setting suppress_warnings to `True` in Client or AsyncClient.")
 
 BASE_URL = 'https://api.pgamerx.com'
 IMAGE_TYPES = ['aww', 'duck', 
