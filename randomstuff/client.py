a6057a2fac069420e0d6ff4cd328281756853416
NerdGuyAhmad <thisisizharahmad@gmail.com>
2021-06-02 17:11:24 +0500

    Deprecated v2 from sync

--- randomstuff/client.py
+++ randomstuff/client.py
@@ -24,7 +24,7 @@ class Client:
 	close(): Closes the session.
 
 	"""
-	def __init__(self, key: str, version: str = '3', suppress_warnings: bool = False):
+	def __init__(self, key: str, version: str = '4', suppress_warnings: bool = False):
 		if not version in VERSIONS:
 			raise VersionError("Invalid API version was provided. Use `2` or `3` only.")
 			return
@@ -35,20 +35,13 @@ class Client:
 		self.session = requests.Session()
 		self.session.headers.update({'x-api-key': self.key})
 		
-		if self.version == 'v2':
-			_warn(self, 'You are using v2 of API which is outdated and will soon be deprecated. To avoid feature issues and unstability issues, Please set `version` to "3" to use latest and supported version.\n')
-		elif self.version == 'v4':
-			_warn(self, "You are using v4 as your API version which is currently in beta state. You may face issues and errors.")
-
-	def get_ai_response(self,
-		message: str,
-		lang: str = 'en',
-		type: str = 'stable',
-		plan: str = '',
-		dev_name: str = 'PGamerX',
-		bot_name: str = 'RSA',
-		unique_id: str = ''
-		):
+		if self.version == 'v3':
+			_warn(self, 'You are using v3 of API. Version 4 is out with improvements. Please migrate as soon as possible.\n')
+		
+	def get_ai_response(self, 
+		message:str, 
+		plan:str='', 
+		**kwargs):
 		"""Gets AI response
 
 		Parameters:
@@ -73,24 +66,45 @@ class Client:
 			raise PlanError(F"Invalid Plan. Choose from {PLANS}")
 			return
 
-		if self.version == 'v4':
-			raise VersionError("v4 is a beta version with entirely different endpoint and parameters. Please use `Client.get_ai_response_beta()` for v4 until beta is over.")
+		if kwargs['server'] and not kwargs['server'] in SERVERS:
+			raise ServerError(f"Invalid server type choose from {SERVERS}.") 
 			return
 
-		if self.version == 'v2' and plan != None:
-			raise VersionError(f"{self.version} does not support {plan} plan.")
-			return
-		
-		if self.version == 'v2':
-			response = self.session.get(f'{BASE_URL}/ai/response?message={message}&language={lang}&api_key={self.key}')
-			return response.json()[0]
+		if self.version == 'v3':
+			try:
+				params = {
+				'message': message, 
+				'lang': kwargs.get('lang', 'en'), 
+				'type': kwargs.get('type', 'stable'), 
+				'bot_name': kwargs.get('bot_name', 'RSA'), 
+				'dev_name': kwargs.get('dev_name', 'PGamerX'),
+				'unique_id': kwargs.get('unique_id', ''),
+				'plan': plan
+				}
+			response = self.session.get(f'{BASE_URL}/v3/ai/response', params=params)
+			if response.status_code == 401:
+				raise AuthError(response.text)
+				return
 
-		params = {'message': message, 
-		'lang': lang, 
-		'type': type, 
-		'bot_name': bot_name, 
-		'dev_name': dev_name,
-		'unique_id': unique_id}
+			return response.json()[0]['message']
+
+		elif self.version == 'v4':
+			params = {
+			'message': message, 
+			'server': kwargs.get('server', 'primary'), 
+			'master': kwargs.get('master', 'PGamerX'), 
+			'bot': kwargs.get('bot', 'RSA'), 
+			'uid': kwargs.get('uid', ''), 
+			'language': kwargs.get('language', 'en'), 
+			'plan': plan
+			}
+			response = self.session.get(f'{BASE_URL}/v4/ai/response', params=params)
+			if response.status_code == 401:
+				raise AuthError(response.text)
+				return
+
+			return response.json()[0]['message']
+
 
 		if self.version == 'v3' and plan == None:
 			response = self.session.get(f'{BASE_URL}/v3/ai/response', params=params)
