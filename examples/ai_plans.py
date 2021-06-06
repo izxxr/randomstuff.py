eedbc696aaa401605d1c016a3b59f2c6ac12e33d
NerdGuyAhmad <thisisizharahmad@gmail.com>
2021-06-06 18:21:18 +0500

    Update normal plan

--- examples/ai_plans.py
+++ examples/ai_plans.py
@@ -9,17 +9,17 @@ client = randomstuff.Client(key="api-key-here") # Input the API key that has the
 
 """Below are examples for all the plans choose the one for the plan you have"""
 
-response = client.get_ai_response(message="The message", plan=None) # No plan (Normal aka Free)
-print(response)
+response = client.get_ai_response(message="The message", plan='') # No plan (Normal aka Free)
+print(response.message)
 
 response = client.get_ai_response(message="The message", plan="pro") # Pro plan
-print(response)
+print(response.message)
 
 response = client.get_ai_response(message="The message", plan="ultra") # Ultra plan
-print(response)
+print(response.message)
 
 response = client.get_ai_response(message="The message", plan="biz") # Biz plan
-print(response)
+print(response.message)
 
 response = client.get_ai_response(message="The message", plan="mega") # Mega plan
-print(response)
+print(response.message)
