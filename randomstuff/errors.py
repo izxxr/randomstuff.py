0ded7f4a1127a3772a6c8e2adc5d9a32e9369ead
NerdGuyAhmad <thisisizharahmad@gmail.com>
2021-06-16 18:05:48 +0500

    Added status_code attribute for HTTPError

--- randomstuff/errors.py
+++ randomstuff/errors.py
@@ -1,67 +1,70 @@
 class HTTPError(Exception):
-	"""Super class for 500s status codes.
+    """Super class for 500s status codes.
 
-	This usually indicates that library couldn't establish a connection with base URL (or API). This is raised
-	when API is has something wrong.
-	"""
-	pass
+    This usually indicates that library couldn't establish a connection with base URL (or API). This is raised
+    when API is has something wrong.
+    """
+    def __init__(self, message, status_code):
+        super().__init__(message)
+        self.message = message
+        self.status_code = status_code
 
 class ArgumentError(Exception):
-	"""Super class for argument related errors"""
-	pass
+    """Super class for argument related errors"""
+    pass
 
 class InvalidType(Exception):
-	"""Raised upon invalid (unsupported) type of parameter provided"""
-	pass
+    """Raised upon invalid (unsupported) type of parameter provided"""
+    pass
 
 class InvalidVersionError(ArgumentError):
-	"""
-	Inherits from `ArgumentError`
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    """
+    Inherits from `ArgumentError`
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	Raised upon invalid version passed
-	"""
-	pass
+    Raised upon invalid version passed
+    """
+    pass
 
 class InvalidPlanError(ArgumentError):
-	"""
-	Inherits from `ArgumentError`
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    """
+    Inherits from `ArgumentError`
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	Raised upon invalid plan passed
-	"""
-	pass
+    Raised upon invalid plan passed
+    """
+    pass
 
 class InvalidServerError(ArgumentError):
-	"""
-	Inherits from `ArgumentError`
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    """
+    Inherits from `ArgumentError`
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	Raised upon invalid server passed
-	"""
-	pass
+    Raised upon invalid server passed
+    """
+    pass
 
 
 class Forbidden(Exception):
-	"""Super class for 403, 401 and other related status codes."""
-	pass
+    """Super class for 403, 401 and other related status codes."""
+    pass
 
 class PlanNotAllowed(Exception):
-	"""
-	Inherits from `Forbiddem`
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    """
+    Inherits from `Forbiddem`
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	Raised when the plan provided is not allowed on the API key. This is usually because you don't have that plan
-	bought. 
-	"""
-	pass
+    Raised when the plan provided is not allowed on the API key. This is usually because you don't have that plan
+    bought. 
+    """
+    pass
 
 class BadAPIKey(Exception):
-	"""
-	Inherits from `Forbiddem`
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    """
+    Inherits from `Forbiddem`
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	Raised when the API key provided is not valid.
-	"""
-	pass
+    Raised when the API key provided is not valid.
+    """
+    pass
 
