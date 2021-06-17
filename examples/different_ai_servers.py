269e059190cb58a804827cbc979fa20f77e34154
Izhar Ahmad <54180221+nerdguyahmad@users.noreply.github.com>
2021-06-17 12:45:08 +0500

    Update different_ai_servers.py

--- examples/different_ai_servers.py
+++ examples/different_ai_servers.py
@@ -5,5 +5,5 @@ Warning! Use primary for general and backup if primary is down. unstable will ha
 """
 import randomstuff
 
-client= randomstuff.Client(key="api-key-here")
-client.get_ai_response(message="Hi", server="primary") # Pass in type in server argument. Choose from `primary`, `backup` and `unstable`.
+with randomstuff.Client(key="api-key-here") as client:
+  client.get_ai_response(message="Hi", server="primary") # Pass in type in server argument. Choose from `primary`, `backup` and `unstable`.
