b086d2ed14e1f573839cbce2197f801af305c91c
Izhar Ahmad <54180221+nerdguyahmad@users.noreply.github.com>
2021-06-17 12:44:40 +0500

    Add context manager example

--- examples/ai_plans.py
+++ examples/ai_plans.py
@@ -5,21 +5,19 @@ RandomStuff.py supports all type of plans for AI response endpoint.
 """
 import randomstuff
 
-client = randomstuff.Client(key="api-key-here") # Input the API key that has the paid plan. Please note that you cannot use plans with version 2 of API!
+with randomstuff.Client(key="api-key-here") as client: # Input the API key that has the paid plan. Please note that you cannot use plans with version 2 of API!
+  """Below are examples for all the plans choose the one for the plan you have"""
+  response = client.get_ai_response(message="The message", plan='') # No plan (Normal aka Free)
+  print(response.message)
 
-"""Below are examples for all the plans choose the one for the plan you have"""
+  response = client.get_ai_response(message="The message", plan="pro") # Pro plan
+  print(response.message)
 
-response = client.get_ai_response(message="The message", plan='') # No plan (Normal aka Free)
-print(response.message)
+  response = client.get_ai_response(message="The message", plan="ultra") # Ultra plan
+  print(response.message)
 
-response = client.get_ai_response(message="The message", plan="pro") # Pro plan
-print(response.message)
+  response = client.get_ai_response(message="The message", plan="biz") # Biz plan
+  print(response.message)
 
-response = client.get_ai_response(message="The message", plan="ultra") # Ultra plan
-print(response.message)
-
-response = client.get_ai_response(message="The message", plan="biz") # Biz plan
-print(response.message)
-
-response = client.get_ai_response(message="The message", plan="mega") # Mega plan
-print(response.message)
+  response = client.get_ai_response(message="The message", plan="mega") # Mega plan
+  print(response.message)
