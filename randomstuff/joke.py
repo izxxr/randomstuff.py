0a5abbadae33614f7f4d0ae10589100c3a954de0
NerdGuyAhmad <thisisizharahmad@gmail.com>
2021-07-17 15:20:30 +0500

    [joke object] Move JokeFlags above Joke

--- randomstuff/joke.py
+++ randomstuff/joke.py
@@ -1,6 +1,41 @@
 from typing import List, Union, Optional
 from dataclasses import dataclass
 
+
+@dataclass(frozen=True)
+class JokeFlags:
+    """
+    Represents a joke's flags
+
+    Attributes
+    ----------
+
+      nsfw : bool
+        Determines if the joke is marked NSFW or not.
+
+      religious : bool
+        Determines if the joke is marked religious or not.
+
+      political : bool
+        Determines if the joke is marked political or not.
+
+      racist : bool
+        Determines if the joke is marked racist or not.
+
+      sexist : bool
+        Determines if the joke is marked sexist or not.
+
+      explicit : bool
+        Determines if the joke is marked explicit or not.
+
+    """
+    nsfw: bool = None
+    religious: bool = None
+    political: bool = None
+    racist: bool = None
+    sexist: bool = None
+    explicit: bool = None
+
 @dataclass(frozen=True)
 class Joke:
     """
@@ -44,38 +79,4 @@ class Joke:
     lang: str = None
 
     # Aliasing
-    language: str = None
-
-@dataclass(frozen=True)
-class JokeFlags:
-    """
-    Represents a joke's flags
-
-    Attributes
-    ----------
-
-      nsfw : bool
-        Determines if the joke is marked NSFW or not.
-
-      religious : bool
-        Determines if the joke is marked religious or not.
-
-      political : bool
-        Determines if the joke is marked political or not.
-
-      racist : bool
-        Determines if the joke is marked racist or not.
-
-      sexist : bool
-        Determines if the joke is marked sexist or not.
-
-      explicit : bool
-        Determines if the joke is marked explicit or not.
-
-    """
-    nsfw: bool = None
-    religious: bool = None
-    political: bool = None
-    racist: bool = None
-    sexist: bool = None
-    explicit: bool = None
+    language: str = lang
