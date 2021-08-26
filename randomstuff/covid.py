760e0131f17ee88a293f5897ef4cd42b8bb3482b
Izhar Ahmad <54180221+nerdguyahmad@users.noreply.github.com>
2021-08-26 18:33:45 +0500

    Update covid.py

--- randomstuff/covid.py
+++ randomstuff/covid.py
@@ -34,12 +34,42 @@ class ClosedCasesPercentage:
 
 @dataclass(frozen=True)
 class ClosedCases:
+    """
+    Represents the closed cases of a country COVID-19 data.
+    
+    Attributes
+    ----------
+    
+    percentage: ClosedCasesPercentage
+        The percentage of closed cases.
+    
+    total: str
+        The total closed cases.
+    """
     percentage: ClosedCasesPercentage = None
     total: str = None
 
 
 @dataclass(frozen=True)
 class CountryCovidData:
+    """
+    Represents the COVID-19 data of a country.
+    
+    This is generally returned when a country is specified in :meth:``get_covid_data`` method.
+    
+    Attributes
+    ----------
+    
+    country: Country
+        The country related to the data.
+
+ 
+    cases: Cases
+        The cases count.
+    
+    closed_cases: ClosedCases
+        The closed cases count.
+    """
     country: Country = None
     cases: Cases = None
     closed_cases: ClosedCases = None
