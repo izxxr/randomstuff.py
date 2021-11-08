d81a701c7610c77b4bcf38a477dc9afb3adde93d
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-11-08 23:39:36 +0530

    Helper functions to validate required image/text

--- randomstuff/_helper.py
+++ randomstuff/_helper.py
@@ -49,3 +49,35 @@ def _check_status(response) -> None:
             f"An error occured while connecting to the API. Returned with status code: {status}",
             status=status,
         )
+
+def _get_method_images(method: str):
+    final = {key: 1 for key in ONE_IMAGE_METHODS}
+    for key in TWO_IMAGE_METHODS:
+        final[key] = 2
+    for key in THREE_IMAGE_METHODS:
+        final[key] = 3
+    for key in TEXT_METHODS:
+        final[key] = 4
+    return final[method]
+
+def _validate_method_image(method, **kwargs):
+    if method not in ALL_METHODS:
+        raise ValueError(
+            "Method not supported. "
+            "Visit https://api-docs.pgamerx.com/Canvas/optional-customisation/ for valid methods"
+        )
+
+    img1, img2, img3, txt = (
+        kwargs.get(i) for i in ["img1", "img2", "img3", "txt"]
+    )
+
+    images = _get_method_images(method)
+
+    if (images == 1) and (img1 is None):
+        raise ValueError(f"img1 is required for method {method}")
+    if (images == 2) and (not all([img1, img2])):
+        raise ValueError(f"img1 and img2 are required for method {method}")
+    if (images == 3) and (not all([img1, img2. img3])):
+        raise ValueError(f"img1, img2 and img3 are required for method {method}")
+    if (images == 4) and (txt is None):
+        raise ValueError(f"txt is required for method {method}")
