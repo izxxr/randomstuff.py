0a82610f6afe27e330a857eff42f0a2f906a2c64
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-11-08 22:17:00 +0530

    Format file

--- randomstuff/_helper.py
+++ randomstuff/_helper.py
@@ -1,23 +1,33 @@
 import inspect
 from colorama import init
 from .errors import *
+from .constants import *
+
 
 def _warn(client, warning) -> None:
     if client.suppress_warnings:
         return
-    else:
-        init()
-        print("\u001b[33m"+"[WARNING] "+warning)
-        print("\u001b[36m"+"\n[INFO] Disable warnings by setting suppress_warnings to `True` in client." + "\u001b[0m")
+    init()
+    print("\u001b[33m" + "[WARNING] " + warning)
+    print(
+        "\u001b[36m"
+        + "\n[INFO] Disable warnings by setting suppress_warnings to `True` in client."
+        + "\u001b[0m"
+    )
+
 
 def _check_coro(client) -> None:
     """Private method to initiate warning if the enivornment is asynchronus"""
     tup = inspect.stack()[2]
     try:
         if inspect.iscoroutinefunction(tup[0].f_globals[tup[3]]):
-            _warn(client, "It seems you're using randomstuff.Client in an async enivornment. It is strongly recommended that you use randomstuff.AsyncClient to avoid blocking functions.")
+            _warn(
+                client,
+                "It seems you're using randomstuff.Client in an async enivornment. It is strongly recommended that you use randomstuff.AsyncClient to avoid blocking functions.",
+            )
     except KeyError:
         return
+
 
 def _check_status(response) -> None:
     try:
@@ -30,9 +40,12 @@ def _check_status(response) -> None:
 
     elif status == 403:
         raise PlanNotAllowed(response.text)
-    
+
     elif status == 429:
         raise RateLimited(response.text)
 
     elif status >= 500:
-        raise HTTPError(f"An error occured while connecting to the API. Returned with status code: {status}", status=status)
+        raise HTTPError(
+            f"An error occured while connecting to the API. Returned with status code: {status}",
+            status=status,
+        )
