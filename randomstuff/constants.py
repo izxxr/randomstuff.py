27f5fb146a513144a393122407748025661c231e
Marcus <77537491+MarzaElise@users.noreply.github.com>
2021-11-08 21:13:41 +0530

    Format file

--- randomstuff/constants.py
+++ randomstuff/constants.py
@@ -1,17 +1,24 @@
-BASE_URL = 'https://api.pgamerx.com'
+BASE_URL = "https://api.pgamerx.com"
 
-IMAGE_TYPES = ['aww', 'duck', 
-	'dog', 'cat', 
-	'memes', 'dankmemes', 
-	'holup', 'art', 
-	'harrypottermemes','facepalm']
-JOKE_TYPES = ['any', 'dev', 'spooky', 'pun']
-WAIFU_TYPES = ['waifu', 'neko', 'shinobu', 'megumin', 'bully', 'cuddle']
+IMAGE_TYPES = [
+    "aww",
+    "duck",
+    "dog",
+    "cat",
+    "memes",
+    "dankmemes",
+    "holup",
+    "art",
+    "harrypottermemes",
+    "facepalm",
+]
+JOKE_TYPES = ["any", "dev", "spooky", "pun"]
+WAIFU_TYPES = ["waifu", "neko", "shinobu", "megumin", "bully", "cuddle"]
 
-PLANS = ['', 'pro', 'ultra', 'biz', 'mega'] # Order lowest -> highest
+PLANS = ["", "pro", "ultra", "biz", "mega"]  # Order lowest -> highest
 
-VERSIONS = ['3', '4', '5']
-DISCONTINUED_VERSIONS = ['2'] # Order: oldest -> newest
+VERSIONS = ["3", "4", "5"]
+DISCONTINUED_VERSIONS = ["2"]  # Order: oldest -> newest
 
-SERVERS_V4 = ['primary', 'backup', 'unstable']
-SERVERS_V5 = ['main', 'backup']
+SERVERS_V4 = ["primary", "backup", "unstable"]
+SERVERS_V5 = ["main", "backup"]
